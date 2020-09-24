f16cf3b47e66979da54b657128bf621bc5f9db56
Denizalp <denizalp.goktas@outlook.com>
2020-09-23 22:37:33 -0400

    Finished Summary Plots Widget

--- fisherApp/app.R
+++ fisherApp/app.R
@@ -29,20 +29,20 @@ get_prices <- function(budgets, valuations, utilities, rho) {
   } else if (utilities == "Leontief") {
     # Leontief Prices
     # Variables to solve for
-    allocations <- Variable(num_buyers, num_goods)
-    utils <- Variable(num_buyers)
+    price <- Variable(num_goods)
+    # allocations <- Variable(num_buyers, num_goods)
+    # utils <- Variable(num_buyers)
     
     # Common constraints across all programs for all utilities
-    constraint1 <- sum_entries(allocations, axis = 2) <= 1
-    constraint2 <- allocations >= 0  
-    constraint3 <- utils <= min_entries(allocations/valuations, axis = 1)
+    # constraint1 <- sum_entries(allocations, axis = 2) <= 1
+    constraint <- price >= 0  
+    # constraint3 <- utils <= min_entries(allocations/valuations, axis = 1)
     
     # Program
-    objective <- Maximize(t(budgets) %*% log(utils))
-    problem <- Problem(objective, constraints = list(constraint1, constraint2, constraint3))
+    objective <- Minimize(sum(price) - t(budgets) %*% log(valuations %*% price))
+    problem <- Problem(objective, constraints = list(constraint))
     result <- solve(problem)
-    prices <- result$getDualValue(constraint1)
-    
+    prices <- result$getValue(price)
   } else if (utilities == "CES") {
     # CES Prices
     # Variables to solve for
@@ -62,103 +62,97 @@ get_prices <- function(budgets, valuations, utilities, rho) {
     
   } else {
     # Cobb-Douglas prices
-    prices <- t(valuations) %*% budgets
+    v <- apply(valuations, MARGIN = 1, function(x) x/sum(x))
+    prices <- v %*% budgets
   }
   return(prices)
 }
 
-
-# First Page
-firstPage <- sidebarLayout(
-  sidebarPanel(
-    navlistPanel(
-      tabPanel("Buyer 1",
-               numericInput("value1buyer1",
-                            "Value for good 1 of buyer 1:",
-                            value = 0.5,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("value2buyer1",
-                            "Value for good 2 of buyer 1:",
-                            value = 0.3,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("value3buyer1",
-                            "Value for good 3 of buyer 1:",
-                            value = 0.2,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("budget1",
-                            "Budget for buyer 1:",
-                            value = 0.5,
-                            min = 0,
-                            max = 1,
-                            step = 0.01)
-      ),
-      tabPanel("Buyer 2",
-               numericInput("value1buyer2",
-                            "Value for good 1 of buyer 2:",
-                            value = 0.5,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("value2buyer2",
-                            "Value for good 2 of buyer 2:",
-                            value = 0.3,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("value3buyer2",
-                            "Value for good 3 of buyer 2:",
-                            value = 0.2,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("budget2",
-                            "Budget for buyer 2:",
-                            value = 0.3,
-                            min = 0,
-                            max = 1,
-                            step = 0.01)
-      ),
-      tabPanel("Buyer 3",
-               numericInput("value1buyer3",
-                            "Value for good 1 of buyer 3:",
-                            value = 0.5,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("value2buyer3",
-                            "Value for good 2 of buyer 3:",
-                            value = 0.3,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("value3buyer3",
-                            "Value for good 3 of buyer 3:",
-                            value = 0.2,
-                            min = 0,
-                            max = 1,
-                            step = 0.01),
-               numericInput("budget3",
-                            "Budget for buyer 3",
-                            value = 0.2,
-                            min = 0,
-                            max = 1,
-                            step = 0.01)
-      )
-    )
+inputsFirstPage <- navlistPanel(
+  tabPanel("Values",
+           flowLayout(
+           column( width = 12, h3("Buyer 1"),
+           sliderInput("value1buyer1",
+                       "Good 1:",
+                       value = 0.5,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("value2buyer1",
+                       "Good 2:",
+                       value = 0.3,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("value3buyer1",
+                       "Good 3:",
+                       value = 0.2,
+                       min = 0,
+                       max = 1,
+                       step = 0.01)),
+           column( width = 12, 
+                   h3("Buyer 2"),      
+           sliderInput("value1buyer2",
+                       "Good 1:",
+                       value = 0.5,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("value2buyer2",
+                       "Good 2:",
+                       value = 0.3,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("value3buyer2",
+                       "Good 3:",
+                       value = 0.2,
+                       min = 0,
+                       max = 1,
+                       step = 0.01)),
+           column( width = 12, 
+                   h3("Buyer 3"),      
+           sliderInput("value1buyer3",
+                       "Value for good 1:",
+                       value = 0.5,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("value2buyer3",
+                       "Value for good 2:",
+                       value = 0.3,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("value3buyer3",
+                       "Value for good 3:",
+                       value = 0.2,
+                       min = 0,
+                       max = 1,
+                       step = 0.01)
+           ))
   ),
-  
-  # Show a plot of the equilibrium prices
-  mainPanel(
-    plotOutput("summaryPlot"),
-    tableOutput("prices")
+  tabPanel("Budgets",
+           sliderInput("budget1",
+                       "Buyer 1:",
+                       value = 0.5,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("budget2",
+                       "Buyer 2:",
+                       value = 0.3,
+                       min = 0,
+                       max = 1,
+                       step = 0.01),
+           sliderInput("budget3",
+                       "Buyer 3",
+                       value = 0.2,
+                       min = 0,
+                       max = 1,
+                       step = 0.01)
   )
-)
+) 
 
 # Second page
 secondPage <- sidebarLayout(
@@ -262,10 +256,15 @@ ui <- fluidPage(
    
    # Application title
    titlePanel("Fisher Market Equilibrium Prices for Market with 3 Goods and 3 Buyers"),
-   tabsetPanel(
-     tabPanel("Summary Plots", firstPage),
-     tabPanel("CES Fisher Markets", secondPage)
-     )
+   fluidRow(column(width = 5, h3("Equilibrium Prices"), tableOutput("prices")),
+            column(width = 5, plotOutput("summaryPlot", height = 250))
+   ),
+   fluidRow(inputsFirstPage)
+   
+   # tabsetPanel(
+   #   tabPanel("Summary Plots", firstPage),
+   #   tabPanel("CES Fisher Markets", secondPage)
+   #   )
 )
 
 # Define server logic required to draw a histogram
@@ -282,18 +281,21 @@ server <- function(input, output) {
      cd_prices <- round(get_prices(budgets, valuations, "cobb-douglas"), 3)
      linear_prices <-  round(get_prices(budgets, valuations, "linear"), 3)
      leontief_prices <-  round(get_prices(budgets, valuations, "Leontief"), 3)
-     
+    
+     prices_output <- cbind(c("1", "2", "3"), cd_prices, linear_prices, leontief_prices)
+     rownames(prices_output) <- c("1", "2", "3")
+     colnames(prices_output) <- c("Goods", "Cobb-Douglas", "Linear", "Leontief")
      # Set dataframe   
      prices <- rbind(cd_prices, linear_prices, leontief_prices)
-     prices_data <- data.frame(cbind(prices, rep(paste("Good", 1:num_goods), 3)), c(rep("Cobb-Douglas", num_goods), rep("Linear", num_goods), rep("Leontief", num_goods)))
-     colnames(prices_data) <- c("Prices", "Good", "Utilities")
+     prices_data <- data.frame(cbind(c(rep("Cobb-Douglas", num_goods), rep("Linear", num_goods), rep("Leontief", num_goods)), rep(1:num_goods, 3)), prices)
+     colnames(prices_data) <- c("Utilities", "Goods", "Prices")
      
-     output$prices <- renderTable(prices_data)
+     output$prices <- renderTable(prices_output)
      # Return Plot
-     return(ggplot(data = prices_data, aes(fill = Utilities, y = Prices, x = Good)) + geom_col(position = "dodge") )
+     return(ggplot(data = prices_data, aes(fill = Utilities, y = Prices, x = Goods)) + geom_col(position = "dodge") )
  
    })
-   
+ 
    output$cesPlot <- renderPlot({
      # Set inputs
      budgets <- c(input$CESbudget1, input$CESbudget2, input$CESbudget3)
@@ -308,7 +310,7 @@ server <- function(input, output) {
      colnames(prices_data) <- c("Prices", "Good")
      
      # Return Plot
-     return(ggplot(data = prices_data, aes(fill = Good, y = Prices, x = Good)) + geom_col(position = "dodge") )
+     return(ggplot(data = prices_data, aes(fill = Goods, y = Prices, x = Goods)) + geom_col(position = "dodge") )
      
    })
 }
